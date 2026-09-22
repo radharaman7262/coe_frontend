04a4c05ff88a74f690d8311f63db54d44a0fa5f5
Radha Raman <119476390+radharaman7262@users.noreply.github.com>
2026-09-22 10:31:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     turbopack: {},
     /* config options here */
+    output: 'standalone',
+    
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
